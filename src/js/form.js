f8d0101aee0452efdbda0670b1525bf56a1ac2a9
Gustavo Rodrigues <Gustavorr001@gmail.com>
2022-10-06 19:47:22 -0300

    alterando o responsivo

--- src/js/form.js
+++ src/js/form.js
@@ -1,75 +1,8 @@
-
-// const forms = document.querySelectorAll('[required]');
-// console.log(forms)
-// const btn = document.querySelector('button');
-
-// import { forms, btn } from './variables.js';
-// import { formSent, formNotSent } from './sendMessage.js';
-
-
-// function checkForms(forms) {
-//     forms.forEach( form => {
-//         checkEmptyForm(form);
-//     });
-// }
-
-// function checkEmptyForm(form) {
-//     if (form.value.trim() === '') {
-//         setErrorFor(form, "Campo Obrigatório");
-//     } else {
-//         setSuccessFor(form);
-//     }
-// }
-
-
-// function applyError(element) {
-//     element.classList.add('error');
-//     element.classList.remove('success');
-// }
-
-// function applySuccess(element) {
-//     element.classList.add('success');
-//     element.classList.remove('error');
-// }
-
-// function setSuccessFor (input) {
-//     const formControl = input.parentElement;
-//     applySuccess(formControl);
-// }
-
-// function setErrorFor(input, message){
-//     const formControl = input.parentElement;
-//     const errorMessage = formControl.querySelector('small');
-
-//     applyError(formControl);
-//     errorMessage.innerHTML = message;
-// }
-
-// btn.addEventListener('click', e => {
-//     e.preventDefault();
-//     checkForms(forms);
-// });
-
-// forms.forEach( form => {
-//     form.addEventListener('click', () => {
-//         checkEmptyForm(form);
-//     });
-// });
-
-// factory
-
-// const form = document.getElementById('form')
-// const userName = document.getElementById('userName');
-// const email = document.getElementById('email');
-// const phone = document.getElementById('phone');
-
-
 import { form, userName, email, phone, emailFormat, phoneFormat } from './variables.js';
 
 form.addEventListener('click',(e) => {
     e.preventDefault();
     checkInputs(form);
-
 });
 
 function checkInputs() {
@@ -95,7 +28,6 @@ function checkInputs() {
         setErrorFor(phone, 'Esse número não é válido')
     }
 }
-
 
 function applyError(element) {
     element.classList.add('error');
